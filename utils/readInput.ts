caacabad124fa17f09a7d5d142d4ae60339809e6
mufarodev <mufaro.priv@gmail.com>
2025-12-03 02:48:31 +0100

    forgot to push remaining code

--- utils/readInput.ts
+++ utils/readInput.ts
@@ -1,4 +1,6 @@
 export default async function readInput() {
   const open = Bun.file(`${process.cwd()}/input.txt`);
-  return await open.text().then((text) => text.split("\r\n").filter((v) => v));
+  return (
+    await open.text().then((text) => text.split("\n").filter((v) => v))
+  ).flat();
 }
---
+++ utils/solution.ts
@@ -0,0 +1,45 @@
+import readInput from "./readInput";
+
+type RunFunction = (solution: Solution) => void;
+
+export class Solution {
+  public solution = 0;
+  public input: string[] = [];
+  public tests: Array<Array<string>> = [];
+  public title = "";
+
+  constructor(public run: RunFunction) {
+    this.runAsync();
+  }
+
+  public describe(newTitle: string) {
+    this.title = newTitle;
+  }
+
+  public setTests(tests: [string[]]) {
+    this.tests = tests;
+  }
+
+  private async runAsync() {
+    this.input = await readInput();
+    const start = performance.now();
+    this.run(this);
+    const end = performance.now();
+    console.log(
+      `${this.title && `[${this.title}]`} Solution: ${this.solution} (took ${(end - start).toFixed(2)} ms)`,
+    );
+
+    if (this.tests.length > 0) {
+      for (const [index, testInput] of this.tests.entries()) {
+        this.solution = 0;
+        this.input = testInput;
+        const testStart = performance.now();
+        this.run(this);
+        const testEnd = performance.now();
+        console.log(
+          `  [${this.title}:test-${index + 1}] Solution: ${this.solution} (took ${(testEnd - testStart).toFixed(2)} ms)`,
+        );
+      }
+    }
+  }
+}
